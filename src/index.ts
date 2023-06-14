c420b0ce10ebd8427f2a8d61f04b46e38d67c702
Racz Kerry <raczkerry@gmail.com>
2023-06-14 13:05:09 +0200

    feat: add locale argument to addTerms

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import { getDiff } from 'json-difference'
 import { readFileSync } from 'fs'
 import { TraduoraClient } from './traduora-client'
 
-export async function addTerms(translationsPath: string, url: string, projectId: string, clientId: string, clientSecret: string) {
+export async function addTerms(translationsPath: string, url: string, projectId: string, clientId: string, clientSecret: string, locale: string) {
   let translations = JSON.parse(readFileSync(translationsPath, 'utf-8'))
 
   const Traduora = new TraduoraClient({ url, projectId, clientId, clientSecret })
 
   await Traduora.login()
 
-  const en = await Traduora.getTranslations('en')
+  const en = await Traduora.getTranslations(locale)
 
   const { added } = getDiff(en, translations, true)
 
@@ -17,7 +17,7 @@ export async function addTerms(translationsPath: string, url: string, projectId:
     added.map(([key, value]) =>
       (async () => {
         const term = await Traduora.addTerm(key)
-        await Traduora.addTranslation(term.id, 'en', value)
+        await Traduora.addTranslation(term.id, locale, value)
         console.log(`Term ${key} added`)
       })()
     )
